bdecbf42511189c518da7ba201be80373061b2b3
Identityofsine <67929513+Identityofsine@users.noreply.github.com>
2023-09-13 18:32:11 -0400

    changed spelling and shit

--- demos/virtual-controller/main.cpp
+++ demos/virtual-controller/main.cpp
@@ -5,7 +5,6 @@
 
 #pragma comment(lib, "setupapi.lib")
 
-
 void run() {
   // Initialize the client context
   const auto client = vigem_alloc();
@@ -18,10 +17,11 @@ void run() {
   }
 
   // send a handle to our driver
-  const auto return_value = vigem_connect(client);
-  if (!VIGEM_SUCCESS(return_value)) {
+  const auto bus_return_value = vigem_connect(client);
+  if (!VIGEM_SUCCESS(bus_return_value)) {
     // print error code out in hex
-		std::cout << "Failed to connect to driver! Error: " << std::hex << return_value << std::endl;
+    std::cout << "Failed to connect to driver! Error: " << std::hex
+              << bus_return_value << std::endl;
     return;
   } else {
     printf("Congrats!, You are connected to driver!\n");
@@ -31,12 +31,13 @@ void run() {
   // initialize controller
   //
   const auto pad = vigem_target_x360_alloc(); // allocate memory for controller
-	
+
   //
   // push pad to vigemBus
   //
-  const auto pad_return_value = vigem_target_add(client, pad); // push controller to vigemBus)
-	
+  const auto pad_return_value =
+      vigem_target_add(client, pad); // push controller to vigemBus)
+
   //
   // check if controller was pushed to vigemBus
   //
@@ -48,26 +49,26 @@ void run() {
     printf("Virtual pad added to client!\n");
   }
 
-	//
-	//pause execution until user-input
-	//
-	printf("Press any key to disconnect...\n");
-	getchar();
-	printf("Disconnecting from driver...\n");
+  //
+  // pause execution until user-input
+  //
+  printf("Press enter to disconnect...\n");
+  getchar();
+  printf("Disconnecting from driver...\n");
 
-	//
-	//disconnect pads from computer, free resources (disconnect virtual device)
-	//
- 	vigem_target_remove(client, pad);	
-	//
-	// free Memory
-	//
-	vigem_target_free(pad); 
-	//
-	//adding a sleep to demonstrate driver disconnection
-	//
-	Sleep(2000);
-	printf("Disconnecting from bus...\n");
+  //
+  // disconnect pads from computer, free resources (disconnect virtual device)
+  //
+  vigem_target_remove(client, pad);
+  //
+  // free Memory
+  //
+  vigem_target_free(pad);
+  //
+  // adding a sleep to demonstrate driver disconnection
+  //
+  Sleep(2000);
+  printf("Disconnecting from bus...\n");
   //
   // finish up and disconnect from driver
   //
@@ -76,15 +77,14 @@ void run() {
   // free memory
   //
   vigem_free(client);
-
-	printf("Exit!\n");
+  printf("Exit!\n");
 }
 
 int main() {
-	//
-	//welcome message
-	//
-	printf("Hello World, Controller Virtualization!\n");
+  //
+  // welcome message
+  //
+  printf("Hello World, Controller Virtualization!\n");
   run();
   return 0;
 }
