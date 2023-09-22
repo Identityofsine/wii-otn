287adb3628a5d0a61ce4fd559ff92e4b3bf76c9b
Identityofsine <67929513+Identityofsine@users.noreply.github.com>
2023-09-22 10:45:30 -0400

    added expriemental conser. state

--- wiiotn-client/src/obj/state.ts
+++ wiiotn-client/src/obj/state.ts
@@ -31,4 +31,29 @@ function createState<T>(default_value: T | undefined = undefined) {
 
 }
 
+export function useConservativeState<T>(default_value: T | undefined) {
+	const current_state = createState(default_value);
+
+	const setState = (new_value: T) => {
+		/* compare current_state and previous_state and create a new object that only contains changed data*/
+		type TJSON = { [key: string]: any };
+		let new_state: T | TJSON = { ...current_state.getState() };
+
+		Object.keys(new_state as TJSON).forEach(key => {
+			if ((new_state as TJSON)[key] === (current_state.getState() as TJSON)[key])
+				delete (new_state as TJSON)[key];
+		});
+
+		current_state.setState({ ...new_value });
+	}
+
+	return {
+		getState: current_state.getState,
+		setState,
+		addListener: current_state.addListener,
+		removeListener: current_state.removeListener
+	}
+
+}
+
 export default createState;
