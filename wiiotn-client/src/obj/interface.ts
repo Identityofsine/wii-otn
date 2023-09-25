4bdcd269aa7c2a202c83c6b9e1c716d62b1fd0b9
Identityofsine <67929513+Identityofsine@users.noreply.github.com>
2023-09-25 14:59:29 -0400

    client comments

--- wiiotn-client/src/obj/interface.ts
+++ wiiotn-client/src/obj/interface.ts
@@ -1,6 +1,9 @@
 import { Axis, ControllerAxis } from "../renderer/hooks/useGamePad";
 import { ControllerSettings } from "../storage";
 
+/*
+*	@summary Interface for the WIIOTNMessage Model, this is a protocol for the communication between the server and the client
+*/
 export default interface WIIOTNMessage {
 	type: 'connection' | 'controller' | 'ping' | 'disconnect',
 	name: string,
@@ -8,6 +11,9 @@ export default interface WIIOTNMessage {
 	id: number,
 }
 
+/*
+	@summary Currenlty unused, but will be used for the ping modulation.
+*/
 export interface WIIOTNPing {
 	type: 'ping',
 	name: string,
@@ -16,23 +22,10 @@ export interface WIIOTNPing {
 }
 
 /*
- *{
-	"id": number,
-	"name": string,
-	"type": "controller",
-	"controller": "keyboard" | "xbox",
-	"lThumbX": number,
-	"lThumbY": number,
-	"rThumbX": number,
-	"rThumbY": number,
-	"buttons_pressed": number,
-	"lTrigger": number,
-	"rTrigger": number
-}
- *
+ * @summary Interface for all WIIOTN compatible controllers, this is used to transmit the controller data to the server. 
+ * @extends WIIOTNMessage
  *
  */
-
 export interface WIIOTNController extends WIIOTNMessage {
 	id: number,
 	name: string,
@@ -45,6 +38,7 @@ export interface WIIOTNController extends WIIOTNMessage {
 	new: boolean,
 }
 
+//empty controller model
 export const empty_wii_controller: WIIOTNController = {
 	type: 'controller',
 	controller: 'keyboard',
@@ -63,7 +57,6 @@ export const empty_wii_controller: WIIOTNController = {
 };
 
 //map of buttons_pressed
-//
 export const key_map: { [key: number]: number } = {
 	65: 1,
 	66: 2,
@@ -75,18 +68,26 @@ export const key_map: { [key: number]: number } = {
 	39: 128,
 	8: 256,
 	13: 512,
+	16: 1024,
+	17: 2048,
+	18: 4096,
+	32: 8192,
+	27: 16384,
+	9: 32768,
+	192: 65536,
 };
 
 
-//basically recreate key_map 
+//convert a ControllerSettings object into a key_map object, shown above this function
+//@summary this is used to convert the settings into a format that can be used by the controller, we use this on the configure page
+//@warning this will not work if settings.key_map is not a valid key_map object
 export const mapSettingsToController = (settings: ControllerSettings): { [key: number]: number } => {
-	//swap keys and values
+
+	if (!settings.key_map) return {};
+	//swap key and value
 	const key_map = Object.entries(settings.key_map).reduce((acc, [key, value]) => {
 		acc[value] = key as unknown as number;
 		return acc;
 	}, {} as { [key: number]: number });
-
-
-
 	return key_map;
 }
