573ec132d0130c8917cfcdc4c1c0769802674491
Identityofsine <67929513+Identityofsine@users.noreply.github.com>
2023-09-29 23:39:20 -0400

    added queue

--- wiiotn-client/src/obj/socket.ts
+++ wiiotn-client/src/obj/socket.ts
@@ -9,6 +9,39 @@ export interface SocketInterface {
 	sendMessage: (message: WIIOTNMessage) => void;
 }
 
+class Queue {
+	private queued_events: Function[] = [];
+	private is_processing: boolean = false;
+
+	constructor() {
+
+	}
+
+	enqueue(event: Function) {
+		this.queued_events.push(event);
+		this.q_start();
+	}
+
+	dequeue() {
+		this.queued_events.shift();
+	}
+
+	q_start() {
+		if (this.is_processing) return;
+		this.is_processing = true;
+		for (let i = 0; i < this.queued_events.length; i++) {
+			if (this.queued_events.length === 0) break;
+			const found_function = this.queued_events.pop();
+			if (found_function) {
+				found_function();
+			}
+		}
+		this.is_processing = false;
+	}
+
+
+}
+
 /**
  * @summary Simple Socket Object that implements the SocketInterface, this keeps the connection going and sends messages to the server
  * @warning This is not intended to be used in the frontend/renderer, only in the backend/main(electron)
@@ -17,20 +50,24 @@ export default class WIISocket implements SocketInterface {
 	private socket: Socket;
 	private ip: string;
 	private port: number;
-	private queue: WIIOTNMessage[] = [];
+	private socket_queue: Queue;
 
 	constructor(ip: string, port: number) {
 		//construct socket
 		this.socket = createSocket('udp4');
 		this.ip = ip;
 		this.port = port;
+		this.socket_queue = new Queue();
 	}
 
+
 	sendMessage(message: WIIOTNMessage, onError?: (err: Error) => void) {
-		this.socket.send(JSON.stringify(message), this.port, this.ip, (err) => {
-			if (err) {
-				if (onError) onError(err);
-			}
+		this.socket_queue.enqueue(() => {
+			this.socket.send(JSON.stringify(message), this.port, this.ip, (err) => {
+				if (err) {
+					if (onError) onError(err);
+				}
+			});
 		});
 	}
 
